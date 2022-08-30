33c73a6e68e0944bda46d2faf440ab7d3c4b0aab
Kieran Proctor <kieran.lee.proctor@gmail.com>
2022-08-30 09:46:49 +0100

    chore: stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 
 import Meta from '@/components/Meta';
 import Main from '@/layouts/Main';
+import { AppConfig } from '@/utils/AppConfig';
 
 interface Props {}
 
@@ -10,8 +11,8 @@ const Index: NextPage<Props> = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+          title={`Welcome | ${AppConfig.site_name}`}
+          description={AppConfig.description}
         />
       }
     >
@@ -28,6 +29,24 @@ const Index: NextPage<Props> = () => {
 
         <section className="mt-10">
           <h2 className="text-3xl font-medium">Languages &amp; Tech</h2>
+          <p className="mt-4 max-w-[75%] text-lg">
+            These are all of the languages / technologies that I am somewhat
+            proficient in :).
+          </p>
+          <ol className="mt-2">
+            <li>Next.js</li>
+            <li>C#</li>
+            <li>Laravel</li>
+            <li>Tailwind CSS</li>
+          </ol>
+        </section>
+
+        <section className="mt-10">
+          <h3 className="text-3xl font-medium">Projects</h3>
+          <p className="mt-4 max-w-[75%] text-lg">
+            Below is a list of my current top projects, click on a card to find
+            out more.
+          </p>
         </section>
       </div>
     </Main>
